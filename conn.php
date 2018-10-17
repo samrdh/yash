4af2506f9177019e4fe399795986ae5ec3b280c0
samrdh <samrdhgoa@gmail.com>
2018-10-17 13:27:03 +0530

    con2

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
 function OpenConn()
 {
-define('DB_HOST', 'localhvbost');
-define('DB_USER', 'rooot');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME','yash');
 $con=new mysqli(DB_HOST, DB_USER, DB_PASSWORD) or die("Connect failed: %s\n". $con -> error);
