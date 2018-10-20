6e70992cb02f9a99f7123f71bfc0f4ca206d8b6f
dahlinwildling <dahlincarneiro01@gmail.com>
2018-10-20 11:39:07 +0530

    point input pg done

--- pointsettings.php
+++ pointsettings.php
@@ -46,40 +46,52 @@ include("header.php");
                                           <td class="pt-3-half">Gold</td>
                                           <td class="pt-3-half">12</td>
                                           <td class="pt-3-half">0.15</td>
-                                          <td><button class="btn btn-info editbtn">Edit</button></td>
+                                          <td><button type="button" class="btn btn-info" onclick="show1();">Edit</button></td>
                                         </tr>
-                                        <tr>
+                                        <tr id="id1" style="display: none;">
                                           <td class="pt-3-half">Gold</td>
                                           <td class="pt-3-half"><input type="text" class="form-control" placeholder="Percentage"></td>
                                           <td class="pt-3-half"><input type="text" class="form-control" placeholder="Rate Per Point"></td>
-                                          <td><button class="btn btn-success">Done</button></td>
+                                          <td><button type="submit" class="btn btn-success">Done</button></td>
                                         </tr>
 
                                         <tr>
                                           <td class="pt-3-half">Silver</td>
                                           <td class="pt-3-half">10</td>
                                           <td class="pt-3-half">0.10</td>
-                                          <td><button class="btn btn-info editbtn">Edit</button></td>
+                                          <td><button type="button" class="btn btn-info" onclick="show2();">Edit</button></td>
                                         </tr>
-                                        <tr>
+                                        <tr id="id2" style="display: none;">
                                           <td class="pt-3-half">Silver</td>
                                           <td class="pt-3-half"><input type="text" class="form-control" placeholder="Percentage"></td>
                                           <td class="pt-3-half"><input type="text" class="form-control" placeholder="Rate Per Point"></td>
-                                          <td><button class="btn btn-success">Done</button></td>
+                                          <td><button type="submit" class="btn btn-success">Done</button></td>
                                         </tr>
-                                        <tr > 
+                                        <tr> 
                                           <td class="pt-3-half">Bronze</td>
                                           <td class="pt-3-half">5</td>
                                           <td class="pt-3-half">0.5</td>
-                                          <td><button class="btn btn-info editbtn"  type="button" onclick="myFunction()">Edit</button></td>
+                                          <td><button type="button" class="btn btn-info" onclick="show3();">Edit</button></td>
                                         </tr> 
-                                        <tr id="point">
+                                        <tr id="id3" style="display: none;">
                                           <td class="pt-3-half" >Bronze</td>
                                           <td class="pt-3-half"><input type="text" class="form-control" placeholder="Percentage"></td>
                                           <td class="pt-3-half"><input type="text" class="form-control" placeholder="Rate Per Point"></td>
-                                          <td><button class="btn btn-success" >Done</button></td>
+                                          <td><button type="submit" class="btn btn-success">Done</button></td>
                                         </tr>
                                          
+                                        <script>
+                                            function show1() { 
+                                              document.getElementById("id1").style.display="table-row";
+                                            }
+                                            function show2() {
+                                              document.getElementById("id2").style.display="table-row";
+                                            }
+                                            function show3() {
+                                              document.getElementById("id3").style.display="table-row";
+                                            }
+                                        </script>
+
                                         </tbody>
                                       </table>
                                                                       
