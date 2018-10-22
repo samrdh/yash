10ab4800a83f5b07de164fece60262dacd05fee9
samrdh <samrdhgoa@gmail.com>
2018-10-22 15:27:08 +0530

    index changed

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
   session_start();
   include"conn.php";
@@ -79,44 +78,6 @@
                </form>
                
 					
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login</title>
-      <!-- Bootstrap Core CSS -->
-      <link href="css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Custom CSS -->
-<link href="css/sb-admin.css" rel="stylesheet">
-
-
-<!-- Custom Fonts -->
-<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-
-<div class="row">
-<div class="col-lg-4">
-              <div class="card bg-primary text-center card-form">
-                <div class="card-body">
-                  <h3>Login</h3>
-                  <form>
-                    <div class="form-group">
-                      <input type="text" class="form-control form-control-lg" placeholder="Username">
-                    </div>
-
-                    <div class="form-group">
-                      <input type="password" class="form-control form-control-lg" placeholder="Confirm Password">
-                    </div>
-                    <input type="submit" class="btn btn-outline-light btn-block">
-                  </form>
-                </div>
-              </div>
->>>>>>> 74d030dae05ff4f0f8a9f89ef155d663ec437948
             </div>
 </div>
 
